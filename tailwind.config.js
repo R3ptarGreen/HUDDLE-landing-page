29f9a28e589bdfd0e8923bd5cf9bd46df85fc0fc
Edgar <jorge260590@hotmail.com>
2023-08-31 13:27:05 -0500

    feat: Primer commit - Configuración inicial del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        pink: 'hsl(322, 100%, 66%)',
+        paleCyan: 'hsl(193, 100%, 96%)',
+        darkCyan: 'hsl(192, 100%, 9%)',
+        grayishBlue: 'hsl(208, 11%, 55%)',
+      },
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: 400,
+        bold: 700,
+        semiBold: 600,
+      },
+      fontSize: {
+        base: '1.125rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
